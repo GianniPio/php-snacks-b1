99eafa80d5c55da6bbc3de196944f91c96675c60
GianniPio <giannipiopellegrino00@gmail.com>
2021-12-20 18:27:56 +0100

    snack_5 completed

---
+++ snack_5/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+    $str = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, minus deserunt quod illum neque odit quasi eaque a obcaecati rerum vel commodi recusandae. Sed laborum provident earum dolorem molestiae nesciunt. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, minus deserunt quod illum neque odit quasi eaque a obcaecati rerum vel commodi recusandae. Sed laborum provident earum dolorem molestiae nesciunt. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, minus deserunt quod illum neque odit quasi eaque a obcaecati rerum vel commodi recusandae. Sed laborum provident earum dolorem molestiae nesciunt.";
+
+    $paragraf= (explode(".",$str));
+
+    for ($i=0; $i < count($paragraf) ; $i++) {
+        echo $paragraf[$i] . "<br><br>";
+
+    }
+    
+    ?>
+</body>
+</html>

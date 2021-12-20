36b759e959fcfd37d1fa242ba0c87cc351d6e0e2
GianniPio <giannipiopellegrino00@gmail.com>
2021-12-20 16:10:15 +0100

    add variabili

---
+++ snack_2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Verifica accesso</title>
+
+
+    <?php
+    // variabili per i parametri da passare
+    $name= $_GET["name"];
+    $mail= $_GET["mail"];
+    $age= $_GET["age"];
+
+    ?>
+
+</head>
+<body>
+ 
+    
+</body>
+</html>

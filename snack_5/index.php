4c1727d183627dcbc171dc6798a72e22d68421f2
GianniPio <giannipiopellegrino00@gmail.com>
2021-12-21 11:56:57 +0100

    add ciclo foreach

--- snack_5/index.php
+++ snack_5/index.php
@@ -8,15 +8,20 @@
     <title>Document</title>
 </head>
 <body>
+    <h1>Snack 5</h1>
 
     <?php
     $str = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, minus deserunt quod illum neque odit quasi eaque a obcaecati rerum vel commodi recusandae. Sed laborum provident earum dolorem molestiae nesciunt. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, minus deserunt quod illum neque odit quasi eaque a obcaecati rerum vel commodi recusandae. Sed laborum provident earum dolorem molestiae nesciunt. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, minus deserunt quod illum neque odit quasi eaque a obcaecati rerum vel commodi recusandae. Sed laborum provident earum dolorem molestiae nesciunt.";
 
-    $paragraf= (explode(".",$str));
+    $paragrafs= (explode(".",$str));
 
-    for ($i=0; $i < count($paragraf) ; $i++) {
-        echo $paragraf[$i] . "<br><br>";
+    // for ($i=0; $i < count($paragraf) ; $i++) {
+    //     echo $paragraf[$i] . ".<br><br>";
 
+    // }
+
+    foreach ($paragrafs as $paragraf) {
+        echo $paragraf . "<br><br>";
     }
     
     ?>
